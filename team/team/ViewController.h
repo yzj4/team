d6bf385bdb0fca9c9b78beadbd2179caf2bc8ffc
l k j y <yzj19932@sina.com>
2016-03-01 16:12:53 +0800

    ff

---
+++ team/team/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  team
+//
+//  Created by l k j y on 16/3/1.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ team/team/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  team
+//
+//  Created by l k j y on 16/3/1.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
